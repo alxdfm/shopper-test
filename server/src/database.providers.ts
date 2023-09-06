0879277e3312aa6115110010535f0f8d6fa16bca
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-09-06 01:23:37 -0300

    configure database and env variables

---
+++ server/src/database.providers.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import db from './config/databaseConfig';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: db().host,
+        port: Number(db().port),
+        username: db().user,
+        password: db().password,
+        database: db().name,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
