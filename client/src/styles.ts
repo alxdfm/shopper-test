a3ee18a7432797ef3c5579649a44fbfb5e59eb01
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-09-07 03:50:49 -0300

    start basic frontend reading file

---
+++ client/src/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 1080px;
+  margin-top: 16px;
+  border-radius: 8px;
+  background-color: #0dab77;
+  color: #eee;
+`;
