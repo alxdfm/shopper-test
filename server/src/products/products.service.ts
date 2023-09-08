b25883891fa239e3139014f95a0454587a35bc79
Alexandre Machado <alexandre_fmachado@hotmail.com>
2023-09-08 12:32:40 -0300

    improve products service

--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -37,40 +37,45 @@ export class ProductsService {
     return array;
   }
 
+  async getProducts(productsCode) {
+    return await this.productRepository.find({
+      where: this.mountWhereCode(productsCode, 'code'),
+    });
+  }
+
+  async getPacks(productsCode, findKey) {
+    return await this.packsRepository.find({
+      where: this.mountWhereCode(productsCode, findKey),
+    });
+  }
+
   async validate(updateData: UpdatePriceDto[]): Promise<void | ErrorType> {
     try {
-      //extract product code from array
       const getProductsCode = this.getProductsCode(updateData);
 
-      //find the match products and packs
-      const productResult = await this.productRepository.find({
-        where: this.mountWhereCode(getProductsCode, 'code'),
-      });
-      const packResultProduct = await this.packsRepository.find({
-        where: this.mountWhereCode(getProductsCode, 'productId'),
-      });
+      const productResult = await this.getProducts(getProductsCode);
 
-      const packResultPack = await this.packsRepository.find({
-        where: this.mountWhereCode(getProductsCode, 'packId'),
-      });
+      const packResultProduct = await this.getPacks(
+        getProductsCode,
+        'productId',
+      );
+
+      const packResultPack = await this.getPacks(getProductsCode, 'packId');
 
       const packResult = [...packResultPack, ...packResultProduct];
 
-      //validate if exists a pack and the products included
       if (!packIncludesProduct(packResult, getProductsCode)) {
         ErrorFactory(
           'Há uma atualização de preço de pacotes sem atualização de preço de produtos.',
         );
       }
 
-      //validate array is not empty
       if (!arrayIsNotEmpty(productResult)) {
         ErrorFactory(
           'Não foram encontrados produtos para os dados informados.',
         );
       }
 
-      //validate the sum of products in a pack
       const packNoDuplicate = removeDuplicated(packResultPack);
       for (const pack of packNoDuplicate) {
         const filter = packResultPack.filter(
@@ -88,13 +93,12 @@ export class ProductsService {
           .filter((product) => product.code === data.productCode)
           .at(0);
 
-        //validate if the sales if grater than cost
         if (!salesGreaterCost(data.newPrice, filter.costPrice)) {
           ErrorFactory(
             'O preço de venda de algum de seus produtos é maior que o custo do produto',
           );
         }
-        //verify the max reajust according marketing
+
         const percentage = 10;
         if (maxReajustPrice(percentage, filter.salesPrice, data.newPrice)) {
           ErrorFactory(
